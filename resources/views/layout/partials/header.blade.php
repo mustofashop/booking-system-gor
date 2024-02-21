5aad0198a1eef5446de0737e2bb711cbbb2bc033
dodis233 <dodis233>
2024-02-21 13:02:38 +0700

    #003 | Update Front End | DS

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -4,9 +4,13 @@
 
         <!-- Uncomment below if you prefer to use an image logo -->
         <div class="logo">
-            <h1><a href="#"><span>
-                        <img src="{{asset('assets/img/logo-white.svg')}}">
-                    </span></a></h1>
+            <h1>
+                <a href="#">
+                    <span>
+                        <img src="{{asset('assets/img/logo-white.svg')}}" alt="logo" class="img-fluid">
+                    </span>
+                </a>
+            </h1>
         </div>
 
         @include('layout.partials.nav')
