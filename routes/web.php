558c1352947f67c0971ce24f4870084ca77742b4
dodis233 <dodis233>
2024-03-13 13:38:37 +0700

    #000 | Update Bug and Permission | DS

--- routes/web.php
+++ routes/web.php
@@ -156,40 +156,40 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/confirm/{id}/edit', [ConfirmController::class, 'edit'])->name('confirm.edit');
         Route::put('/confirm/{id}', [ConfirmController::class, 'update'])->name('confirm.update');
         Route::delete('/confirm/destroy/{id}', [ConfirmController::class, 'destroy'])->name('confirm.destroy');
+
+        // Member Management
+        Route::get('/member', [MemberController::class, 'index'])->name('member.index');
+        Route::get('/member/create', [MemberController::class, 'create'])->name('member.create');
+        Route::post('/member', [MemberController::class, 'store'])->name('member.store');
+        Route::get('/member/show/{id}', [MemberController::class, 'show'])->name('member.show');
+        Route::get('/member/{id}/edit', [MemberController::class, 'edit'])->name('member.edit');
+        Route::put('/member/{id}', [MemberController::class, 'update'])->name('member.update');
+        Route::delete('/member/destroy/{id}', [MemberController::class, 'destroy'])->name('member.destroy');
+
+        // User Management
+        Route::get('/event', [EventController::class, 'index'])->name('event.index');
+        Route::get('/event/create', [EventController::class, 'create'])->name('event.create');
+        Route::post('/event', [EventController::class, 'store'])->name('event.store');
+        Route::get('/event/show/{id}', [EventController::class, 'show'])->name('event.show');
+        Route::get('/event/{id}/edit', [EventController::class, 'edit'])->name('event.edit');
+        Route::put('/event/{id}', [EventController::class, 'update'])->name('event.update');
+        Route::delete('/event/destroy/{id}', [EventController::class, 'destroy'])->name('event.destroy');
     });
 
-    // Member Management
-    Route::get('/member', [MemberController::class, 'index'])->name('member.index');
-    Route::get('/member/create', [MemberController::class, 'create'])->name('member.create');
-    Route::post('/member', [MemberController::class, 'store'])->name('member.store');
-    Route::get('/member/show/{id}', [MemberController::class, 'show'])->name('member.show');
-    Route::get('/member/{id}/edit', [MemberController::class, 'edit'])->name('member.edit');
-    Route::put('/member/{id}', [MemberController::class, 'update'])->name('member.update');
-    Route::delete('/member/destroy/{id}', [MemberController::class, 'destroy'])->name('member.destroy');
-});
+    // Role Event
+    Route::middleware(['event'])->group(function () {
+        Route::get('/profil', ProfilController::class)->name('profil');
 
+        // Point Management
+        Route::get('/point', [PointController::class, 'index'])->name('point.index');
+        Route::get('/point/create', [PointController::class, 'create'])->name('point.create');
+        Route::post('/point', [PointController::class, 'store'])->name('point.store');
+        Route::get('/point/show/{id}', [PointController::class, 'show'])->name('point.show');
+        Route::get('/point/{id}/edit', [PointController::class, 'edit'])->name('point.edit');
+        Route::put('/point/{id}', [PointController::class, 'update'])->name('point.update');
+        Route::delete('/point/destroy/{id}', [PointController::class, 'destroy'])->name('point.destroy');
 
-// Role Event
-Route::middleware(['event'])->group(function () {
-    Route::get('/profil', ProfilController::class)->name('profil');
-
-    // User Management
-    Route::get('/event', [EventController::class, 'index'])->name('event.index');
-    Route::get('/event/create', [EventController::class, 'create'])->name('event.create');
-    Route::post('/event', [EventController::class, 'store'])->name('event.store');
-    Route::get('/event/show/{id}', [EventController::class, 'show'])->name('event.show');
-    Route::get('/event/{id}/edit', [EventController::class, 'edit'])->name('event.edit');
-    Route::put('/event/{id}', [EventController::class, 'update'])->name('event.update');
-    Route::delete('/event/destroy/{id}', [EventController::class, 'destroy'])->name('event.destroy');
-
-    // Point Management
-    Route::get('/point', [PointController::class, 'index'])->name('point.index');
-    Route::get('/point/create', [PointController::class, 'create'])->name('point.create');
-    Route::post('/point', [PointController::class, 'store'])->name('point.store');
-    Route::get('/point/show/{id}', [PointController::class, 'show'])->name('point.show');
-    Route::get('/point/{id}/edit', [PointController::class, 'edit'])->name('point.edit');
-    Route::put('/point/{id}', [PointController::class, 'update'])->name('point.update');
-    Route::delete('/point/destroy/{id}', [PointController::class, 'destroy'])->name('point.destroy');
+    });
 
     // Role Member
     Route::middleware(['member'])->group(function () {
@@ -223,4 +223,6 @@ Route::middleware(['event'])->group(function () {
         Route::put('/payment/{id}', [PaymentController::class, 'update'])->name('payment.update');
         Route::delete('/payment/{id}', [PaymentController::class, 'destroy'])->name('payment.destroy');
     });
+
 });
+
